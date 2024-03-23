a65b4c3c6cd60a55d2a6632080b8cf82b5c50a26
Archit Gupta <architgupta7017@gmail.com>
2024-03-24 02:09:24 +0530

    adding files

---
+++ src/BasicsOfJava/basics.java
@@ -0,0 +1,22 @@
+package BasicsOfJava;
+
+public class basics {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println();
+        System.out.println("Hello World 2");
+        System.out.println("Hello World 3");
+
+        int x;
+        x = 5;
+        System.out.println(x);
+
+        x = 500;
+        System.out.println(x);
+        System.out.println(x-20);
+
+        x = x + 5;
+        System.out.println(x);
+
+    }
+}
---
+++ src/BasicsOfJava/multiple_variables.java
@@ -0,0 +1,17 @@
+package BasicsOfJava;
+
+public class multiple_variables {
+    public static void main(String[] args) {
+        int x = 5, y = 4, z = 1;
+
+        System.out.print("The value of x is ");
+        System.out.println(x);
+
+        System.out.println("The value of y is "+y);
+
+        System.out.println("The value of z is "+z);
+
+        System.out.println("1 hour 15 min se lecture continue karna hai");
+
+    }
+}
